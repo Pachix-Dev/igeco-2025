548b046b6537242f708c5c5278f326c9a0550b9a
Pachi-Dev <pachi.claros@gmail.com>
2025-05-27 00:07:02 -0600

    Add English pages for Areas of Expertise, About Us, Our History, and Home
    - Created `areas-de-experiencia.astro` for displaying areas of expertise in English.
    - Created `index.astro` as the main landing page in English, featuring events and testimonials.
    - Created `nosotros.astro` to provide information about the company in English.
    - Created `nuestra-historia.astro` to narrate the company's history in English.
    - Updated existing Spanish pages to maintain consistency with the new English content.

---
+++ src/data/constans_carousel.js
@@ -0,0 +1,28 @@
+export const carouselhome = [
+        { 
+            image: '/img/ecomondo_es.webp', 
+            image_en: '/img/ecomondo_en.webp',
+            alt: 'ECOMONDO 2026', 
+            link: 'https://ecomondomexico.com.mx' 
+        },
+        {   
+            image: '/img/amof_2025_bg.webp', 
+            image_en: '/img/amof_2025_bg.webp',
+            alt: 'AMOF 2026', 
+            link: 'https://amofexpo.igeco.mx' 
+        },
+        { 
+            image: '/img/replusbg.webp', 
+            image_en: '/img/replusbg_eng.webp',
+            alt: 'REPLUS 2026', 
+            link: 'https://re-plus-mexico.com.mx' 
+        },
+
+        { 
+            image: '/img/itm_2025_primerov2.webp', 
+            image_en: '/img/kv_itm_2025_eng_vf.webp',
+            alt: 'ITM 2025', 
+            link: 'https://industrialtransformation.mx' 
+        },
+        
+    ]
