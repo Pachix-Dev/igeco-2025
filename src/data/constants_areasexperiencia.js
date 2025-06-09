c9dd89d160b729ebb393ee785daa241b4b99e665
Catasi <aideecasillas14@gmail.com>
2025-06-09 13:01:33 -0600

    actualizacion areas experiencia

---
+++ src/data/constants_areasexperiencia.js
@@ -0,0 +1,382 @@
+export const areasExperiencia = [
+  {
+    title: "Industria",
+    title_en: "Industry",
+    text: "¡La fábrica del futuro se puede experimentar en vivo en Hannover!<br /><br /> Industria 4.0, procesos totalmente automatizados y soluciones inteligentes de suministro, tratamientos innovadores de superficies y limpieza: todo esto y más se presenta en nuestras ferias más emblemáticas, por ejemplo:",
+    text_en:
+      "The factory of the future can be experienced live in Hannover!<br /><br /> Industry 4.0, fully automated processes and intelligent supply solutions, innovative surface treatments and cleaning: all this and more is presented at our flagship trade fairs, for example:",
+    image: "/img/areas2.webp",
+    links: [
+      {
+        name: "AUTOMATION & TESTING",
+        href: "https://vicenza.aetevent.com/",
+      },
+      {
+        name: "HANNOVER MESSE",
+        href: "https://www.hannovermesse.de/",
+      },
+      {
+        name: "INDUSTRIAL AUTOMATION SHOW",
+        href: "https://www.industrial-automation-show.com/En/",
+      },
+      {
+        name: "SURFACE TECHNOLOGY GERMANY",
+        href: "https://www.surface-technology-germany.de/en/",
+      },
+      {
+        name: "INDUSTRIAL TRANSFORMATION ASIA-PACIFIC",
+        href: "https://www.industrial-transformation.com/",
+      },
+      {
+        name: "INDUSTRIAL TRANSFORMATION SAUDI ARABIA",
+        href: "https://saudiindustrialtransformation.com/",
+      },
+      {
+        name: "INDUSTRIAL TRANSFORMATION AUSTRALIA",
+        href: "https://www.industrial-transformation.com.au/",
+      },
+      {
+        name: "INDUSTRIAL TRANSFORMATION MEXICO",
+        href: "https://industrialtransformation.mx/en/",
+      },
+      {
+        name: "LASERFAIR Shenzen",
+        href: "https://www.laserfair.cn/index.php?lang=en/",
+      },
+      {
+        name: "LIGNA",
+        href: "https://www.ligna.de",
+      },
+      {
+        name: "parts2clean",
+        href: "https://www.parts2clean.de/en/",
+      },
+      {
+        name: "PTC Asia",
+        href: "https://www.ptc-asia.com/index-en.html",
+      },
+      {
+        name: "South China International Industry Fair",
+        href: "https://www.sciif.com/en/",
+      },
+      {
+        name: "WIN EURASIA",
+        href: "https://www.win-eurasia.com/en",
+      },
+    ],
+  },
+  {
+    title: "Sustentabilidad",
+    title_en: "Sustainability",
+    text: "ECOMONDO es una plataforma internacional líder dedicada a la economía circular y la transición ecológica. Se celebra en distintas regiones del mundo, adaptándose a los contextos locales pero manteniendo su enfoque en la innovación tecnológica y las soluciones sostenibles para la gestión de residuos, el agua, la energía renovable y la movilidad verde.",
+    text_en:
+      "ECOMONDO is a leading international platform dedicated to the circular economy and ecological transition. It is held in different regions of the world, adapting to local contexts but maintaining its focus on technological innovation and sustainable solutions for waste management, water, renewable energy and green mobility.",
+    image: "/img/sustentabilidad.webp",
+    links: [
+      {
+        name: "ECOMONDO ITALIA",
+        href: "https://www.ecomondo.com",
+      },
+      {
+        name: "ECOMONDO CHINA",
+        href: "https://en.ecomondo.com/ecomondo/network/ecomondo-china",
+      },
+      {
+        name: "ECOMONDO MEXICO",
+        href: "https://ecomondomexico.com.mx/en/",
+      },
+    ],
+  },
+  {
+    title: "Energía",
+    title_en: "Energy",
+    text: "Nuestras ferias relacionadas con la energía presentan las soluciones necesarias para satisfacer las necesidades energéticas del futuro en todo el mundo. ¿Cómo difieren las necesidades de los diferentes países? ¿Hasta dónde han llegado las tecnologías y soluciones inteligentes en el sector energético actual? ¿Qué impacto tendrá la digitalización? ¿Qué está pasando con las redes de comunicación inteligente? ¿Cómo se gestionará la producción y el consumo de energía en todo el mundo en el futuro? Si deseas saber cómo está evolucionando el mercado energético y manteneste al día con los últimos desarrollos, nuestras ferias comerciales son imprescindibles, por ejemplo:",
+    text_en:
+      "Our energy-related trade fairs present the solutions needed to meet the energy needs of the future around the world. How do the needs of different countries differ? How far have smart technologies and solutions come in today's energy sector? What impact will digitalization have? What is happening with smart communication networks? How will energy production and consumption be managed around the world in the future? If you want to find out how the energy market is evolving and keep up to date with the latest developments, our trade fairs are a must, for example:",
+    image: "/img/energy.webp",
+    links: [
+      {
+        name: "Electricity Transformation Canada",
+        href: "https://electricity-transformation.ca/",
+      },
+      {
+        name: "THE KEY ENERGY TRANSITION EXPO ",
+        href: "https://en.key-expo.com/",
+      },
+      {
+        name: "RE+ MEXICO",
+        href: "https://re-plus-mexico.com.mx/",
+      },
+      {
+        name: "RE+ PENÍNSULA DE YUCATÁN",
+        href: "https://re-plus-yucatan.igeco.mx/",
+      },
+      {
+        name: "DUBAI MUSCLE SHOW",
+        href: "https://dubaimuscleshow.com/",
+      },
+      {
+        name: "DUBAI ACTIVE",
+        href: "https://dubaiactiveshow.com/",
+      },
+      {
+        name: "RIMINI WELLNESS",
+        href: "https://www.riminiwellness.com/en/",
+      },
+    ],
+  },
+  {
+    title: "Movilidad y Automotriz",
+    title_en: "Movility and Automotriz",
+    text: "Impulsado por la innovación tecnológica, la sostenibilidad y las nuevas demandas de transporte inteligente, este sector abarca desde vehículos convencionales hasta soluciones avanzadas de movilidad urbana, logística de última milla, electromovilidad y transporte público. Diversos eventos internacionales permiten a profesionales, empresas y gobiernos explorar tendencias, compartir conocimientos y establecer alianzas estratégicas.",
+    text_en:
+      "Driven by technological advancements, sustainability and smart transportation demands, this sector covers from conventional vehicles to advanced urban logistics, last-mile delivery, electromobility and public transport. Various international events allow professionals, companies and governments to explore trends, share knowledge and establish strategic alliances.",
+    image: "/img/movility.webp",
+    links: [
+      {
+        name: "AMERICAS’ mobility of the future ",
+        href: "https://amofexpo.igeco.mx/",
+      },
+      {
+        name: "Automechanika ISTANBUL ",
+        href: "https://automechanika-istanbul.tr.messefrankfurt.com/istanbul/en.html/",
+      },
+      {
+        name: "Caravan Bremen",
+        href: "https://www.caravan-bremen.de/",
+      },
+      {
+        name: "IAA TRANSPORTATION ",
+        href: "https://www.iaa-transportation.com/en/",
+      },
+      {
+        name: "IBE INTERMOBILITY AND BUS EXPO ",
+        href: "https://en.expoibe.com/",
+      },
+      {
+        name: "Performance & Style Days",
+        href: "https://www.psdays.com/en/",
+      },
+    ],
+  },
+  {
+    title: "Agroindustria",
+    title_en: "Agroindustria",
+    text: "La Agroindustria es un sector estratégico que integra la producción agrícola con la innovación tecnológica, la sostenibilidad ambiental y el desarrollo económico. Este campo abarca desde la maquinaria agrícola de última generación hasta soluciones para el cultivo, procesamiento, comercialización y distribución de productos agroalimentarios y ornamentales. Diversas ferias internacionales destacan por reunir a los actores clave de esta industria en constante transformación.",
+    text_en:
+      "The Agroindustria is a strategic sector that integrates agricultural production with technological innovation, environmental sustainability and economic development. This field covers from the latest agricultural machinery to solutions for crop cultivation, processing, commercialization and distribution of agricultural products and ornamentals. Various international fairs highlight the key players in this industry in constant transformation.",
+    image: "/img/agroindustria.webp",
+    links: [
+      {
+        name: "AGRITECHNICA",
+        href: "https://www.agritechnica.com/en/",
+      },
+      {
+        name: "FUTURE AG",
+        href: "https://futureagexpo.com.au",
+      },
+      {
+        name: "Myplant & Garden",
+        href: "https://myplantgarden.com/en/",
+      },
+    ],
+  },
+  {
+    title: "Alimentos y Bebidas ",
+    title_en: "Food and Beverages",
+    text: "El sector de Alimentos y Bebidas es una industria dinámica y esencial que combina tradición e innovación para responder a las nuevas demandas de los consumidores en cuanto a calidad, sostenibilidad, experiencias gastronómicas y tecnología de producción. A nivel global, se celebran ferias y exposiciones que impulsan el desarrollo de este sector, promoviendo la internacionalización, el networking y la actualización profesional.",
+    text_en:
+      "The sector of Food and Beverages is a dynamic industry that combines tradition and innovation to meet the new demands of consumers in terms of quality, sustainability, gastronomic experiences and production technology. At a global level, fairs and exhibitions are held that drive the development of this sector, promoting internationalization, networking and professional updates.",
+    image: "/img/bebidas.webp",
+    links: [
+      {
+        name: "BBTech expo",
+        href: "https://en.beerandfoodattraction.it/",
+      },
+      {
+        name: "BEER & GOOD ATTRACTION",
+        href: "https://en.beerandfoodattraction.it/",
+      },
+      {
+        name: "MACFRUT",
+        href: "https://www.macfrut.com/en/",
+      },
+      {
+        name: "SIGEP ASIA",
+        href: "https://sigepasia.com.sg/",
+      },
+      {
+        name: "SIGEP WORLD",
+        href: "https://www.sigep.it/en/",
+      },
+      {
+        name: "RESTAURANT ASIA",
+        href: "https://restaurantasia.com.sg/",
+      },
+      {
+        name: "SPECIALITY ASIA",
+        href: "https://sigepasia.com.sg/",
+      },
+      {
+        name: "VENGING ITALIA",
+        href: "https://venditalia.com/en/",
+      },
+    ],
+  },
+  {
+    title: "Bienestar y Deporte",
+    title_en: "Welfare and Sport",
+    text: "El campo de Bienestar y Deporte abarca un enfoque integral hacia la salud física, mental y emocional, promoviendo estilos de vida activos y equilibrados. Este sector ha crecido significativamente en los últimos años, integrando disciplinas deportivas, entrenamiento personalizado, fitness, nutrición, y bienestar emocional, así como tendencias tecnológicas que optimizan el rendimiento y la calidad de vida.",
+    text_en:
+      "The field of Welfare and Sport covers an integrated approach towards physical health, mental and emotional well-being, promoting active and balanced lifestyles. This sector has grown significantly in the last years, integrating sports training, personalized training, fitness, nutrition, and emotional well-being, as well as technological trends that optimize performance and quality of life.",
+    image: "/img/bienestar-deporte.webp",
+    links: [
+      {
+        name: "THE COACH EXPERIENCE",
+        href: "https://shop.assoallenatori.it/e/27/",
+      },
+      {
+        name: "RIMINI GINNASTICA IN FESTA",
+        href: "https://www.ginnasticainfestarimini.it/",
+      },
+    ],
+  },
+  {
+    title: "Joyería y Relojería ",
+    title_en: "Jewellery and Watchery",
+    text: "El sector de la Joyería y Relojería representa una fusión entre arte, diseño, lujo e innovación. Este campo no solo celebra la excelencia artesanal y la tradición, sino que también integra avances tecnológicos, sostenibilidad y nuevas tendencias en materiales, estilos y experiencias de compra. A nivel internacional, diversas ferias y exposiciones reúnen a diseñadores, fabricantes, minoristas y compradores para impulsar el negocio, la creatividad y el posicionamiento global de las marcas.",
+    text_en:
+      "The field of Jewellery and Watchery represents a fusion between art, design, luxury and innovation. This field not only celebrates the excellence of the arts and tradition, but also integrates technological advances, sustainability and new trends in materials, styles and shopping experiences. At a global level, various fairs and exhibitions gather designers, manufacturers, boutiques and buyers to drive business, creativity and global positioning of brands.",
+    image: "/img/joyas-y-relojes.webp",
+    links: [
+      {
+        name: "JGT DUBAI",
+        href: "https://www.jgtdubaijewelleryshow.com/",
+      },
+      {
+        name: "PALAKISS VICEZA",
+        href: "https://www.palakiss.com/en/fiera-del-gioiello-summer/",
+      },
+      {
+        name: "Singapore International Jewelry Expo",
+        href: "https://sije.com.sg/",
+      },
+      {
+        name: "SUMMIT DEL GIOIELLO ITALIANO",
+        href: "https://www.oroarezzo.it/en/summit-gioiello-italiano/",
+      },
+      {
+        name: "VICEZAORO",
+        href: "https://www.vicenzaoro.com/en/",
+      },
+      {
+        name: "VO’CLOCK PRIVÉ",
+        href: "https://www.vicenzaoro.com/en/vo-clock/",
+      },
+    ],
+  },
+  {
+    title: "Logística",
+    title_en: "Logistics",
+    text: "Montacargas innovadores y ahorradores de energía, vehículos industriales, sistemas de manejo complejos y totalmente automatizados, grúas, equipos de elevación y plataformas hidráulicas, sistemas de estanterías y almacenamiento, los últimos controles y desarrollos de sistemas en el campo de TI de logística, sistemas de identificación automática, logística robótica e ingeniería de envasado para intralogística: todo está a su disposición en HANNOVER MESSE y CeMAT. CeMAT también es un éxito de exportación presente en varios continentes, en forma de:",
+    text_en:
+      "Innovative and energy-saving forklifts, commercial vehicles, complex and fully automated handling systems, cranes, lifting equipment and hydraulic platforms, racking and storage systems, the latest controls and system developments in the field of logistics IT, automatic identification systems, robotic logistics and packaging engineering for intralogistics: everything is at your disposal at HANNOVER MESSE and CeMAT. CeMAT is also an export success on several continents, in the form of:",
+    image: "/img/Logistica.webp",
+    links: [
+      {
+        name: "CeMAT AUSTRALIA",
+        href: "https://www.cemat.com.au/",
+      },
+      {
+        name: "CeMAT ASIA",
+        href: "https://www.cemat-asia.com/index-en.html/",
+      },
+      {
+        name: "CeMAT South East ASIA",
+        href: "https://cematseasia.com/",
+      },
+      {
+        name: "HOW Hub of Warehouse",
+        href: "https://www.hubofwarehouse.com/en",
+      },
+    ],
+  },
+  {
+    title: "Metalmecánica y Metalúrgica",
+    title_en: "Metalworking and Metallurgy",
+    text: "Las ferias comerciales de procesamiento de metales organizadas por Deutsche Messe en todo el mundo sirven como un centro principal para todo el sector al tiempo que destacan las tendencias e innovaciones tecnológicas y promueven los negocios internacionales. Nuestras ferias internacionales para la industria siderúrgica, la fábrica de rollos y la fundición, por un lado, y la industria de procesamiento de metales y trabajo de chapa, por otro lado, ofrecen a las empresas la oportunidad de acceder a nuevos mercados en el mundo.",
+    text_en:
+      "The commercial metalworking and metallurgy fairs organized by Deutsche Messe around the world serve as a major hub for the entire industry while highlighting technological trends and innovations and promoting international business. Our international fairs for the steel industry, the coil mill and foundry, on the one hand, and the metal processing and sheet metal working industry, on the other hand, offer companies the opportunity to access new markets around the world.",
+    image: "/img/metal.webp",
+    links: [
+      {
+        name: "ALUEXPO",
+        href: "https://aluexpo.com/",
+      },
+      {
+        name: "ANKIROS/ TURKCAST",
+        href: "https://www.ankiros.com/",
+      },
+      {
+        name: "Heavy Machinery Asia",
+        href: "https://www.heavymachineryasia.com/index.php?lang=en/",
+      },
+      {
+        name: "Metalworking and CNC Machine Tool Show",
+        href: "https://www.metalworkingchina.com/en/",
+      },
+      {
+        name: "Tornitura Show",
+        href: "https://www.tornitura.show/",
+      },
+    ],
+  },
+  {
+    title: "Recubrimiento para pisos",
+    title_en: "Floor coating",
+    text: "¿Deseas una vista previa de las próximas modas y tendencias en la escena internacional de recubrimiento para pisos? ¿Estás buscando inspiración, ideas de diseño innovadoras, nuevos clientes potenciales o nuevos socios comerciales internacionales? ¡Entonces DOMOTEX es la feria para ti! Cada año, el equipo de DOMOTEX presenta las últimas tendencias nuevas y emergentes en revestimientos de pisos en Hannover, Alemania, y en otros tres centros clave del mercado global:",
+    text_en:
+      "Do you want a preview of the upcoming fashions and trends in the international floor covering scene? Are you looking for inspiration, innovative design ideas, new potential customers or new international business partners? Then DOMOTEX is the trade fair for you! Every year, the DOMOTEX team presents the latest new and emerging trends in floor coverings in Hannover, Germany, and in three other key global market centers:",
+    image: "/img/recubrimientoPiso.webp",
+    links: [
+      {
+        name: "DOMOTEX",
+        href: "https://www.domotex.de/en/",
+      },
+      {
+        name: "DOMOTEX Asia CHINAFLOOR",
+        href: "https://www.domotexasiachinafloor.com/",
+      },
+      {
+        name: "DOMOTEX Middle East",
+        href: "https://www.domotex.de/en/about-us/domotex-worldwide/domotex-middle-east/",
+      },
+    ],
+  },
+  {
+    title: "Seguridad y protección contra incendios",
+    title_en: "Fire safety and protection",
+    text: "La cartera de eventos sobre temas de protección contra incendios, protección preventiva contra incendios, servicios de rescate, protección civil y seguridad continúa creciendo. Además de la feria líder mundial INTERSCHUTZ en Hannover, también estamos presentes en:",
+    text_en:
+      "The portfolio of events on the topics of fire protection, preventive fire protection, rescue services, civil protection and security continues to grow. In addition to the world's leading trade fair INTERSCHUTZ in Hannover, we are also present at:",
+    image: "/img/security.webp",
+    links: [
+      {
+        name: "INTERSCHULTZ",
+        href: "https://www.interschutz.de/en/",
+      },
+      {
+        name: "AFAC Australia",
+        href: "https://www.afacconference.com.au/",
+      },
+      {
+        name: "REAS Italy",
+        href: "https://www.reasonline.it/en/",
+      },
+      {
+        name: "China International Emergency Management Exhibition",
+        href: "https://www.cieme.net/en/",
+      },
+    ],
+  },
+];
