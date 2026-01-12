e2aff9b7a5a63822666129533cdc82b64c2bd80c
JoseZR <josezerrod@hotmail.com>
2026-01-12 10:45:24 -0600

    se elimina a laura amaro del equipo igeco

--- src/data/constans_persons.js
+++ src/data/constans_persons.js
@@ -7,17 +7,17 @@ export const constans_persons = [
       "CEO and Member of the Board of Directors of Italian German Exhibition Company Mexico, USA and Canada.",
     image: "/img/BERND-ICON.webp",
   },
-  // {
-  //   name: "Azul Ogazón",
-  //   position_en: "Deputy Manager IGECO Mexico",
-  //   position: "Deputy Manager IGECO Mexico",
-  //   image: "/img/AZUL-ICON.webp",
-  // },
   {
     name: "Manuel Ferreira",
     position_en: "CFO IGECO Mexico",
     position: "CFO IGECO Mexico",
     image: "/img/manuelferreira.webp",
+  },
+  {
+    name: "Rodolfo Carrizales",
+    position_en: "Director of Finances and Human Resources",
+    position: "Director de Finanzas y Recursos Humanos",
+    image: "/img/RODO-ICON.webp",
   },
   {
     name: "Claudia Rodriguez",
@@ -31,18 +31,6 @@ export const constans_persons = [
     position_en: "Project Manager RE+ Mexico",
     position: "Gerente de Proyecto RE+ Mexico",
     image: "/img/samuel-ramirez.webp",
-  },
-  {
-    name: "Laura Amaro",
-    position_en: "Project Manager Ecomondo Mexico",
-    position: "Gerente de Proyecto Ecomondo Mexico",
-    image: "/img/laura.webp",
-  },
-  {
-    name: "Rodolfo Carrizales",
-    position_en: "Director of Finances and Human Resources",
-    position: "Director de Finanzas y Recursos Humanos",
-    image: "/img/RODO-ICON.webp",
   },
   {
     name: "Lilian Malanco",
